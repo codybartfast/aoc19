e8a813d84e48f5e8e6264b9070129f6c153ac8fd
John Donnellan <codybartfast@gmail.com>
2019-12-03 06:09:59 +0000

    Day 3 Part 2 answer

--- Day03.fs
+++ Day03.fs
@@ -37,50 +37,61 @@ let input =
 (* ================ Part 1 ================ *)
 
 
+let instrs1 = input.[0]
+let instrs2 = input.[1]
+
+let goUp (x, y) = (x + 1, y)
+let goRight (x, y) = (x, y + 1)
+let goDown (x, y) = (x - 1, y)
+let goLeft (x, y) = (x, y - 1)
+
+let expand (dir, count) =
+    let step =
+        match dir with
+        | "U" -> goUp
+        | "R" -> goRight
+        | "D" -> goDown
+        | "L" -> goLeft
+    Seq.init count (fun i -> step)
+
+let apply path inst =
+    (expand inst)
+    |> Seq.fold
+        (fun path f -> (f path.Head)::path)
+        path
+
+let wire instrs =
+    instrs
+    |> Seq.fold apply [0,0]
+    |> List.rev
+        
+let wire1 = wire instrs1
+let wire2 = wire instrs2
+
+let coords1 = wire1 |> Set
+let coords2 = wire2 |> Set
+
+let intersections = 
+    Set.intersect coords1 coords2
+    |> Set.remove (0,0)
 
 let Part1 () =
-    let instrs1 = input.[0]
-    let instrs2 = input.[1]
 
-    let goUp (x, y) = (x + 1, y)
-    let goRight (x, y) = (x, y + 1)
-    let goDown (x, y) = (x - 1, y)
-    let goLeft (x, y) = (x, y - 1)
-
-    let expand (dir, count) =
-        let step =
-            match dir with
-            | "U" -> goUp
-            | "R" -> goRight
-            | "D" -> goDown
-            | "L" -> goLeft
-        Seq.init count (fun i -> step)
-
-    let apply path inst =
-        (expand inst)
-        |> Seq.fold
-            (fun path f -> (f path.Head)::path)
-            path
-
-    let wire instrs =
-        instrs
-        |> Seq.fold apply [0,0]
-            
-    let coords1 = wire instrs1 |> Set
-    let coords2 = wire instrs2 |> Set
-    Set.intersect coords1 coords2
-    |> Set.toSeq
-    |> Seq.sortBy (fun (x, y) -> (abs x) + (abs y))
-    |> Seq.skip 1
-    |> Seq.head
-
-     
-
-
+    intersections    
+    |> Seq.minBy (fun (x, y) -> (abs x) + (abs y))
+    |> snd
 
 
 (* ================ Part 2 ================ *)
 
 
 let Part2 () =
-    ()
+
+    let dist coord =
+        List.findIndex ((=) coord) wire1.Tail
+        + List.findIndex ((=) coord) wire2.Tail
+        + 2
+
+    intersections
+    |> Seq.map dist
+    |> Seq.min
