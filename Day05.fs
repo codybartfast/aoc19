8bbe0833a56ab30b24ff2364c1e06b8de38de95b
John Donnellan <codybartfast@gmail.com>
2019-12-05 08:04:49 +0000

    Day5 refactor

--- Day05.fs
+++ Day05.fs
@@ -1,20 +1,15 @@
 module Day05
 let day = "05"
 
-open System
 open System.IO
-open System.Text.RegularExpressions
-
-let print obj= (printfn "%O" obj)
-let tPrint obj = (print obj); obj
 
 let inputFile = Path.Combine("inputs", "input" + day + ".txt")
 let lines = File.ReadAllLines(inputFile)
 let compile (str: string) = str.Split "," |> Array.map int
-let mutable program = compile lines.[0]
+let program = compile lines.[0]
 
-let mem = Array.copy program
-let reset ()  = program |> Array.iteri (fun i v -> mem.[i] <- v)
+let memory = Array.copy program
+let reset ()  = program |> Array.iteri (fun i v -> memory.[i] <- v)
 
 let mutable sysId = 0
 let readInput () = sysId
@@ -22,105 +17,92 @@ let readInput () = sysId
 let mutable lastOutput = -1
 let writeOutput value = lastOutput <- value
 
-let read addr = mem.[addr]
-let write addr value = mem.[addr] <- value
-let isHalt addr = read addr = 99
+let read addr = memory.[addr]
+let write addr value = memory.[addr] <- value
 
-let instr ptr = mem.[ptr]
+let halt addr = read addr = 99
 
 let posDE n = n % 100
 let posC n = n % 1000 / 100
 let posB n = n % 10000 / 1000
 let posA n = n % 100000 / 10000
 
-let opCode = instr >> posDE
+let opCode = read >> posDE
 
 let arg1 ptr =
-    match (instr >> posC) ptr with
-    | 0 -> mem.[mem.[ptr + 1]]
-    | 1 -> mem.[ptr + 1]
+    match (read >> posC) ptr with
+    | 0 -> read (read (ptr + 1))
+    | 1 -> read (ptr + 1)
     | u -> failwithf "Unexpected mode for first para: %i (ptr: %i)" u ptr
 
 let arg2 ptr =
-    match (instr >> posB) ptr with
-    | 0 -> mem.[mem.[ptr + 2]]
-    | 1 -> mem.[ptr + 2]
+    match (read >> posB) ptr with
+    | 0 -> read (read (ptr + 2))
+    | 1 -> read (ptr + 2)
     | u -> failwithf "Unexpected mode for second para: %i (ptr: %i)" u ptr
 
 let arg3 ptr =
-    match (instr >> posA) ptr with
-    | 0 -> mem.[mem.[ptr + 3]]
-    | 1 -> mem.[ptr + 3]
+    match (read >> posA) ptr with
+    | 0 -> read (read (ptr + 3))
+    | 1 -> read (ptr + 3)
     | u -> failwithf "Unexpected mode for thrid para: %i (ptr: %i)" u ptr
 
-let add ptr = write (read (ptr + 3)) (arg1 ptr + arg2 ptr)
-let mult ptr = write (read (ptr + 3)) (arg1 ptr * arg2 ptr)
-let input ptr = write (read (ptr + 1)) (readInput ())
-let output ptr = writeOutput (arg1 ptr)
-
+let add ptr =
+    (write (read (ptr + 3)) (arg1 ptr + arg2 ptr))
+    (ptr + 4)
+let mult ptr =
+    (write (read (ptr + 3)) (arg1 ptr * arg2 ptr))
+    (ptr + 4)
+let input ptr =
+    (write (read (ptr + 1)) (readInput ()))
+    (ptr + 2)
+let output ptr =
+    writeOutput (arg1 ptr)
+    (ptr + 2)
 let jumpIfTrue ptr =
-    if (arg1 ptr) <> 0
-    then (arg2 ptr)
-    else ptr + 3
+    if (arg1 ptr) <> 0 then (arg2 ptr) else (ptr + 3)
 
 let jumpIfFalse ptr =
-    if (arg1 ptr) = 0
-    then (arg2 ptr)
-    else ptr + 3
+    if (arg1 ptr) = 0 then (arg2 ptr) else ptr + 3
 
 let lessThan ptr =
     write
         (read (ptr + 3))
         (if (arg1 ptr) < (arg2 ptr) then 1 else 0)
+    (ptr + 4)
 
 let equals ptr =
     write
-         (read (ptr + 3))
+        (read (ptr + 3))
         (if (arg1 ptr) = (arg2 ptr) then 1 else 0)
+    (ptr + 4)
 
 let operation ptr =
     match opCode ptr with
-    | 1 -> add, ptr + 4
-    | 2 -> mult, ptr + 4
-    | 3 -> input, ptr + 2
-    | 4 -> output, ptr + 2
-    | 5 -> ignore, jumpIfTrue ptr
-    | 6 -> ignore, jumpIfFalse ptr
-    | 7 -> lessThan, ptr + 4
-    | 8 -> equals, ptr + 4
+    | 1 -> add
+    | 2 -> mult
+    | 3 -> input
+    | 4 -> output
+    | 5 -> jumpIfTrue
+    | 6 -> jumpIfFalse
+    | 7 -> lessThan
+    | 8 -> equals
     | u -> failwithf "Unexpected opCode: %i (ptr: %i)" u ptr
 
 let run id =
     sysId <- id
     let rec execute ptr =
-        if isHalt ptr then
+        if halt ptr then
             ()
         else
-            let (op, next) = operation ptr
-            op ptr
-            execute next
+            execute ((operation ptr) ptr)
     execute 0
 
 let Part1 () =
     run 1
     lastOutput
 
-
-
 let Part2 () =
     reset ()
     run 5
     lastOutput
-    
-    // let ex1a = "3,9,8,9,10,9,4,9,99,-1,8"
-    // program <- compile ex1a
-
-    // reset ()
-    // run 1
-    // printfn "with 1: %i" lastOutput
-    // reset ()
-    // run 8
-    // printfn "with 8: %i" lastOutput
-    // reset ()
-    // run 10
-    // printfn "with 10: %i" lastOutput
