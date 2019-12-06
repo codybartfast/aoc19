101f5b5ee7775fae5834c69da379c86052ab0326
John Donnellan <codybartfast@gmail.com>
2019-12-06 07:34:33 +0000

    Day 6 Part 1 answer

--- Day06.fs
+++ Day06.fs
@@ -12,18 +12,42 @@ let inputFile = Path.Combine("inputs", "input" + day + ".txt")
 let lines = File.ReadAllLines(inputFile)
 
 let parseLine (line: string) =
-    Regex.Match(line, @"(.*)")
-    |> fun (m: Match) ->
-        let grp (idx: int) = m.Groups.[idx].Value
-        let grpi = grp >> int
-        grp 1
+    let parts = line.Split ")"
+    (parts.[0], parts.[1])
 
-let input = lines |> Array.map parseLine
+type Body = {Name: string; Orbits: int; mutable Sats: Body list}
 
+let pairs = lines |> Array.map parseLine |> Set
+// let allLeft = pairs |> List.map fst |> Set
+// let allRight = pairs |> List.map snd |> Set
+
+let bodies = Map [ ("COM", { Name = "COM"; Orbits = 0; Sats =[] }) ]
+
+let addSat (bodies, pairs) =
+    // let pairLen = Set.count pairs
+    let (body, sat) =
+        pairs
+        |> Seq.pick (fun (body, sat) -> 
+            match Map.tryFind body bodies with
+            | None -> None
+            | Some body -> Some (body, sat))
+    let sat = { Name = sat; Orbits = body.Orbits + 1; Sats = []}
+    body.Sats <- sat::body.Sats
+    let bodies = bodies.Add (sat.Name, sat)
+    let pairs = Set.remove (body.Name, sat.Name) pairs
+    (bodies, pairs)
+    // printfn "%i -> %i" pairLen (Map.count pairs)
+
+let rec addSatelites (bodies, pairs) =
+    if Set.isEmpty pairs 
+    then bodies
+    else addSat (bodies, pairs) |> addSatelites
 
 
 let Part1 () =
-    input
+    addSatelites (bodies, pairs)
+    |> Map.toSeq
+    |> Seq.sumBy (snd >> (fun b -> b.Orbits))
 
 
 let Part2 () =
