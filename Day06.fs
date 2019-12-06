045abe88f2ce4751c02a4ff18e21bd4e8de46665
John Donnellan <codybartfast@gmail.com>
2019-12-06 08:13:10 +0000

    Day 6 refactor

--- Day06.fs
+++ Day06.fs
@@ -1,9 +1,7 @@
 module Day06
 let day = "06"
 
-open System
 open System.IO
-open System.Text.RegularExpressions
 
 let print obj= (printfn "%O" obj)
 let tPrint obj = (print obj); obj
@@ -16,17 +14,17 @@ let parseLine (line: string) =
     (parts.[0], parts.[1])
 
 type Body =
-    {Name: string; Axis: string; Orbits: int; mutable Sats: Body list}
+    { Name: string;
+      Axis: string;
+      Orbits: int;
+      mutable Sats: Body list }
 
 let pairs = lines |> Array.map parseLine |> Set
-// let allLeft = pairs |> List.map fst |> Set
-// let allRight = pairs |> List.map snd |> Set
 
-let bodies =
-    Map [ ("COM", { Name = "COM"; Axis = ""; Orbits = 0; Sats =[] }) ]
+let comMap =
+    Map [("COM", { Name = "COM"; Axis = ""; Orbits = 0; Sats =[] })]
 
 let addSat (bodies, pairs) =
-    // let pairLen = Set.count pairs
     let (body, sat) =
         pairs
         |> Seq.pick (fun (body, sat) ->
@@ -34,64 +32,42 @@ let addSat (bodies, pairs) =
             | None -> None
             | Some body -> Some (body, sat))
     let sat =
-        { Name = sat; Axis = body.Name; Orbits = body.Orbits + 1; Sats = []}
+        { Name = sat;
+          Axis = body.Name;
+          Orbits = body.Orbits + 1;
+          Sats = [] }
     body.Sats <- sat::body.Sats
-    let bodies = bodies.Add (sat.Name, sat)
-    let pairs = Set.remove (body.Name, sat.Name) pairs
-    (bodies, pairs)
-    // printfn "%i -> %i" pairLen (Map.count pairs)
+    (bodies.Add (sat.Name, sat))
+    , (Set.remove (body.Name, sat.Name) pairs)
 
-let rec addSatelites (bodies, pairs) =
+let rec buildBodies (bodies, pairs) =
     if Set.isEmpty pairs
     then bodies
-    else addSat (bodies, pairs) |> addSatelites
+    else addSat (bodies, pairs) |> buildBodies
 
-//let orbitCount a b =
-let rec ancestors bodies body =
-    if body.Name = "COM"
-    then ["COM"]
-    else body.Name::(ancestors bodies (Map.find body.Axis bodies))
+let jumpCount a b bodies =
+    let a = Map.find a bodies
+    let b = Map.find b bodies
 
-let rec naiveOrbitCount a ancestors count =
-    match ancestors with
-    | [] -> None
-    | h::_ when a = h -> Some count
-    | _::t -> naiveOrbitCount a t (count + 1)
+    let rec ancestors bodies body =
+        if body.Name = "COM"
+        then ["COM"]
+        else body.Name::(ancestors bodies (Map.find body.Axis bodies))
 
-let orbitCount a b bodies =
     let aAncs = ancestors bodies a
     let bAncs = ancestors bodies b
 
     let rec orbitCount (aAncs: string list) count =
-        match naiveOrbitCount aAncs.Head bAncs count with
-        | Some count -> max 0 (count - 2)
+        match List.tryFindIndex ((=) aAncs.Head) bAncs with
+        | Some c -> c + count
         | None -> orbitCount aAncs.Tail (count + 1)
 
-    orbitCount aAncs 0
-
-let test = Regex.Split (@"COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L
-K)YOU
-I)SAN", @"\r?\n")
+    max 0 ((orbitCount aAncs 0) - 2)
 
 let Part1 () =
-    addSatelites (bodies, pairs)
+    buildBodies (comMap, pairs)
     |> Map.toSeq
     |> Seq.sumBy (snd >> (fun b -> b.Orbits))
 
 let Part2 () =
-    //let pairs = test |> Array.map parseLine |> Set
-    let bodies = addSatelites (bodies, pairs)
-    let santa = bodies.["SAN"]
-    let me = bodies.["YOU"]
-
-    orbitCount me santa bodies
+    jumpCount "YOU" "SAN" (buildBodies (comMap, pairs))
