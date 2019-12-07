a33f58c6f11629bd31b0138eed0c21b05120deba
John Donnellan <codybartfast@gmail.com>
2019-12-07 06:11:58 +0000

    Day 7 Part 1 answered

--- Day07.fs
+++ Day07.fs
@@ -1,30 +1,151 @@
 module Day07
 let day = "07"
 
-open System
 open System.IO
-open System.Text.RegularExpressions
 
 let print obj= (printfn "%O" obj)
 let tPrint obj = (print obj); obj
+let nPrint = id
+
+let rec permutations list =
+    let rec insertAlong i list =
+        match list with
+        | [] -> [[i]]
+        | h::t -> 
+            (i::list)::(List.map (fun sub -> h::sub) (insertAlong i t))
+    match list with
+    | [] -> [[]]
+    | head::tail -> List.collect (insertAlong head) (permutations tail)
 
 let inputFile = Path.Combine("inputs", "input" + day + ".txt")
 let lines = File.ReadAllLines(inputFile)
+let compile (str: string) = str.Split "," |> Array.map int
+let mutable program = compile lines.[0]
 
-let parseLine (line: string) =
-    Regex.Match(line, @"(.*)")
-    |> fun (m: Match) ->
-        let grp (idx: int) = m.Groups.[idx].Value
-        let grpi = grp >> int
-        grp 1
+let memory = Array.copy program
+let reset ()  = program |> Array.iteri (fun i v -> memory.[i] <- v)
 
-let input = lines |> Array.map parseLine
+let mutable lastOutput = -1
+let writeOutput value = lastOutput <- (nPrint value)
 
+let mutable (inputs) = null
+let setInputs [a; b; c; d; e] = 
+    inputs <- (seq{
+             yield a; yield 0;                   
+             yield b; yield (nPrint lastOutput); 
+             yield c; yield (nPrint lastOutput); 
+             yield d; yield (nPrint lastOutput); 
+             yield e; yield (nPrint lastOutput);  })
+        .GetEnumerator ()
 
+let readInput () =
+    inputs.MoveNext () |> ignore
+    inputs.Current
+
+let read addr = memory.[addr]
+let write addr value = memory.[addr] <- value
+
+let halt ptr = read ptr = 99
+
+let posDE n = n % 100
+let posC n = n % 1000 / 100
+let posB n = n % 10000 / 1000
+let posA n = n % 100000 / 10000
+
+let opCode = read >> posDE
+
+let arg1 ptr =
+    match (read >> posC) ptr with
+    | 0 -> read (read (ptr + 1))
+    | 1 -> read (ptr + 1)
+    | u -> failwithf "Unexpected mode for first para: %i (ptr: %i)" u ptr
+
+let arg2 ptr =
+    match (read >> posB) ptr with
+    | 0 -> read (read (ptr + 2))
+    | 1 -> read (ptr + 2)
+    | u -> failwithf "Unexpected mode for second para: %i (ptr: %i)" u ptr
+
+let arg3 ptr =
+    match (read >> posA) ptr with
+    | 0 -> read (read (ptr + 3))
+    | 1 -> read (ptr + 3)
+    | u -> failwithf "Unexpected mode for thrid para: %i (ptr: %i)" u ptr
+
+let add ptr =
+    (write (read (ptr + 3)) (arg1 ptr + arg2 ptr))
+    (ptr + 4)
+
+let mult ptr =
+    (write (read (ptr + 3)) (arg1 ptr * arg2 ptr))
+    (ptr + 4)
+
+let input ptr =
+    (write (read (ptr + 1)) (readInput ()))
+    (ptr + 2)
+
+let output ptr =
+    writeOutput (arg1 ptr)
+    (ptr + 2)
+
+let jumpIfTrue ptr =
+    if (arg1 ptr) <> 0 then (arg2 ptr) else (ptr + 3)
+
+let jumpIfFalse ptr =
+    if (arg1 ptr) = 0 then (arg2 ptr) else ptr + 3
+
+let lessThan ptr =
+    write
+        (read (ptr + 3))
+        (if (arg1 ptr) < (arg2 ptr) then 1 else 0)
+    (ptr + 4)
+
+let equals ptr =
+    write
+        (read (ptr + 3))
+        (if (arg1 ptr) = (arg2 ptr) then 1 else 0)
+    (ptr + 4)
+
+let operation ptr =
+    match opCode ptr with
+    | 1 -> add
+    | 2 -> mult
+    | 3 -> input
+    | 4 -> output
+    | 5 -> jumpIfTrue
+    | 6 -> jumpIfFalse
+    | 7 -> lessThan
+    | 8 -> equals
+    | u -> failwithf "Unexpected opCode: %i (ptr: %i)" u ptr
+
+let run () =
+    reset ()
+    let rec execute ptr =
+       if halt ptr then () else execute ((operation ptr) ptr)
+    execute 0
+
+let runAll settings =
+    setInputs settings
+    run ()
+    // print lastOutput
+    run ()
+    // print lastOutput
+    run ()
+    // print lastOutput
+    run ()
+    // print lastOutput
+    run ()
+    lastOutput
+
+let test1 = @"3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
 let Part1 () =
-    input
+    //program <- compile test1
 
+    [0; 1; 2; 3; 4]
+    |> permutations
+    |> List.map runAll
+    |> List.max
 
 let Part2 () =
     ()
