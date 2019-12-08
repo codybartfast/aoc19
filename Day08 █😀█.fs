7bb26055688f5d7f9b40f45ca9e74f39e96bc424
John Donnellan <codybartfast@gmail.com>
2019-12-08 07:37:13 +0000

    Day 8 refactor

---
+++ Day08 █😀█.fs
@@ -0,0 +1,44 @@
+module Day08
+let day = "08"
+
+open System
+open System.IO
+
+let nl = System.Environment.NewLine
+let filterCount pred = (Array.filter pred >> Array.length)
+
+let inputFile = Path.Combine("inputs", "input" + day + ".txt")
+let lines = File.ReadAllLines(inputFile)
+
+let pixels =
+    lines.[0].ToCharArray ()
+    |> Array.map ((Char.GetNumericValue) >> int)
+
+let width = 25
+let height = 6
+let pixelsPerLayer = width * height
+let depth = pixels.Length / pixelsPerLayer
+
+let layers =
+    [|for z in [0 .. (depth - 1)] ->
+        let start = z * pixelsPerLayer
+        pixels.[start .. (start + pixelsPerLayer - 1)]|]
+
+let render () =
+    let rec renderPixel row col layer =
+        match layers.[layer].[row * 25 + col] with
+        | 2 -> renderPixel row col (layer + 1)
+        | 1 -> "█"
+        | _ -> " "
+
+    String.concat nl
+        [ for row in [0 .. (height - 1)] ->
+            String.concat ""
+                [for col in [0 .. (width - 1)] ->
+                    renderPixel row col 0]]
+
+let Part1 () =
+    let layer = layers |> Array.minBy (filterCount ((=) 0))
+    (filterCount ((=) 1) layer) * (filterCount ((=) 2) layer)
+
+let Part2 () = nl + render () + nl
