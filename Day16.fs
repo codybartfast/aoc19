521f8b02ea35551b9d9cf08118dc267d5e4d5887
John Donnellan <codybartfast@gmail.com>
2019-12-16 09:31:15 +0000

    Day 16 Part 1 answered

--- Day16.fs
+++ Day16.fs
@@ -13,19 +13,54 @@ let readLines day =
     File.ReadAllLines (Path.Combine("inputs", "input" + day + ".txt"))
 let lines = readLines day
 
-let parseLine (line: string) =
-    Regex.Match(line, @"(.*)")
-    |> fun (m: Match) ->
-        let grp (idx: int) = m.Groups.[idx].Value
-        let grpi = grp >> int
-        grp 1
+let parse (str: string) =
+    str.ToCharArray() |> Array.map (int >> ((+) -48)) |> List.ofArray
 
-let input = lines |> Array.map parseLine
+let test1a = "12345678"
+let test1b = "80871224585914546619083218645595"
+let test1c = "19617804207202209144916044189917"
+let test1d = "69317163492948606335995924319873"
 
+let test2a = "03036732577212944063491565474664"
+let test2b = "02935109699940807407585447034323"
+let test2c = "03081770884921959731165446850517"
 
+let input = lines.[0]
+
+let seed = [0; 1; 0; -1]
+let patterns seed len =
+    let rec repeat i = seq { yield i; yield! repeat i}
+    let rec repeatSeq sq = seq{ yield! sq; yield! repeatSeq sq}
+    let stretch sq n = sq |> Seq.collect (repeat >> Seq.take n)
+    [1 .. len]
+    |> Seq.map
+        ((stretch seed) >> repeatSeq >> Seq.tail >> (Seq.take len) >> List.ofSeq)
+    |> List.ofSeq
+
+let apply signal pattern =
+    (List.map2 (*) signal pattern)
+    |> (List.reduce (+) >> abs >> (fun n -> n % 10))
+
+let phase signal =
+    patterns seed (List.length signal)
+    |> List.map (apply signal)
+
+let rec repeatFn fn n input =
+    match n with
+    | 0 -> input
+    | _ -> repeatFn fn (n - 1) (fn input)
+
+let first8 signal =
+    signal
+    |> List.take 8
+    |> List.map (fun i -> i.ToString())
+    |> (String.concat "")
+
+let repeatList n l = [1 .. n] |> List.collect (fun i -> l)
 
 let Part1 () =
-    input
+    let signal = parse input
+    repeatFn phase 100 signal |> first8
 
 let Part2 () =
     ()
