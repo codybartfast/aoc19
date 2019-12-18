962a036dcebc5b61b9b2e40a19f47c5bc80d2883
John Donnellan <codybartfast@gmail.com>
2019-12-18 15:26:56 +0000

    Day 16 Part 2 answered

--- Day16.fs
+++ Day16.fs
@@ -14,7 +14,7 @@ let readLines day =
 let lines = readLines day
 
 let parse (str: string) =
-    str.ToCharArray() |> Array.map (int >> ((+) -48)) 
+    str.ToCharArray() |> Array.map (int >> ((+) -48))
 
 let test1a = "12345678"
 let test1b = "80871224585914546619083218645595"
@@ -31,7 +31,7 @@ let seeds = [0; 1; 0; -1]
 
 let pattern col row =
     match ((col + 1) / (row + 1)) % 4 with
-    | 0 -> 0 | 1 -> 1 | 2 -> 0  | 3 -> -1 
+    | 0 -> 0 | 1 -> 1 | 2 -> 0  | 3 -> -1
 
 let value (signal: int[]) row col = signal.[col] * pattern col row
 
@@ -57,30 +57,35 @@ let first8 output =
     |> Seq.map (fun i -> i.ToString())
     |> (String.concat "")
 
-let repeat n array = [|1 .. n|] |> Array.collect (fun i -> array)
+let repeatArr n array = [|1 .. n|] |> Array.collect (fun i -> array)
 
 let Part1 () =
     let input = (parse input)
     // repeatFn phase 100 signal |> first8
-    (phase input).[input.Length / 2 ..]
-   
-let lastHalf (input: int[]) (output: int[]) =    
-    let len = input.Length
-    output.[len - 1] <- input.[len - 1]
 
-    seq{len - 2 .. -1 .. len - (len / 2)}
-    |> Seq.iter (fun i -> 
-        output.[i] <-  output.[i + 1] + input.[i] |> single)
+    // (phase input).[input.Length / 2 ..]
+    (repeatFn phase 100 input).[input.Length / 2 ..]
+
+let lastPart start (input: int[]) =
+    let len = input.Length
+
+    seq{len - 2 .. -1 .. start}
+    |> Seq.iter (fun i ->
+        input.[i] <-  input.[i + 1] + input.[i] |> single)
+    input
 
 
 let Part2 () =
-    let input = (parse input)    
-    // let input = repeat 100 input
+    let input = (parse test2c)
+    let offset = 
+        input |> Array.take 7 |> Array.map (fun n -> n.ToString())
+        |> String.concat "" |> Int32.Parse
+
+    let mega = repeatArr 10_000 input
+    repeatFn (lastPart offset) 100 mega |> ignore
+    mega.[offset .. offset + 8]
+    |> Array.map (fun n -> n.ToString())
+    |> String.concat ""
 
 
-    let output= Array.zeroCreate input.Length 
-    lastHalf input output
-    output.[output.Length / 2 ..]
-
-    // seq{ 10 .. -1 .. 1 }
-    
+// 538508008 High
