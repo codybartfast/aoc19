6febb7bf2383e462565bbf44ae79cf9a19d131b9
John Donnellan <codybartfast@gmail.com>
2019-12-19 20:27:22 +0000

    d18 new explore

--- Day18.fs
+++ Day18.fs
@@ -19,7 +19,7 @@ let test1d = toLines @"########################
 ########################"
 
 type Grid<'a when 'a : equality>(jagged: 'a[][]) =
-    // aoc15:18    
+    // aoc15:18
     let data = jagged
     let maxX = (Array.length (data.[0])) - 1
     let maxY = (Array.length data) - 1
@@ -73,12 +73,12 @@ type Grid<'a when 'a : equality>(jagged: 'a[][]) =
         Array.append nhood.[0 .. 3] nhood.[5 .. 8]
 
     member this.Bordering(x, y) =
-        [| this.TryGet (x, y - 1); 
-           this.TryGet (x + 1, y); 
-           this.TryGet (x, y + 1); 
+        [| this.TryGet (x, y - 1);
+           this.TryGet (x + 1, y);
+           this.TryGet (x, y + 1);
            this.TryGet (x - 1, y); |]
 
-    member this.Transform<'b  when 'b : equality> 
+    member this.Transform<'b  when 'b : equality>
         (generate: Grid<'a> -> int -> int -> 'b) : Grid<'b> =
             [| for y in 0 .. maxY do
                 [| for x in 0 .. maxX do
@@ -92,7 +92,7 @@ type Grid<'a when 'a : equality>(jagged: 'a[][]) =
         match this.InBounds(x, y) with
         | true -> Some (this.Get((x, y)))
         | false -> None
-        
+
 type Triton = Grid<char>
 
 let tritonGrid =
@@ -113,7 +113,7 @@ let triton lines = tritonGrid (lines)
 type Location = Wall | Clear | Origin | Oxygen
 type Direction = North | South | West | East
 
-let reachable (triton: Triton) start =
+let explore (triton: Triton) start isTarget contOnFind =
     let nextPosition (x, y) dir =
         match dir with
         | North -> (x, y - 1)
@@ -128,79 +128,37 @@ let reachable (triton: Triton) start =
     let reverse = function
         | North -> South | South -> North | West -> East | East -> West
 
-    let rec distances been pos dist =
+    let rec explore been doors pos dist =
         let been = Set.add pos been
         [North; South; West; East]
         |> Seq.map (nextPosition pos)
         |> Seq.filter (been.Contains >> not)
-        |> Seq.collect (fun next ->
+        |> Seq.collect (fun next -> seq{
             match triton.Get next with
-            | '#' -> Seq.singleton None
-            | ltr when Char.IsLetter ltr -> Seq.singleton (Some (ltr, dist))
-            | _ ->
-                distances been next (dist + 1))
+            | '#' -> ()
+            | key when (isTarget key) ->
+                yield (Some ((key, List.rev doors), dist))
+                if contOnFind then yield! explore been doors next (dist + 1)
+            | door when Char.IsUpper door ->
+                yield! explore been (door::doors) next (dist + 1) 
+            | _ -> yield! explore been doors next (dist + 1) })
 
-    distances Set.empty start 0 
+    explore Set.empty [] start 0
     |> Seq.choose id
     |> Seq.groupBy fst
     |> Seq.map (fun (_, keys) -> keys |> Seq.minBy snd)
-    |> List.ofSeq 
-    |> List.partition (fst >> Char.IsLower)
-    //|> (fun (keys, doors) -> (Map keys, Map doors))
+    |> List.ofSeq
 
-type Guide = Map<char,list<char * int> * list<char * int>>
-let guide lines =
-    let triton = triton lines
-    let reachable = reachable triton
-    let start = triton.Find ((=)'@')
-    triton.Set start '.'
-    triton.Filter (Char.IsLetter)
-    |> Seq.map 
-        // ((fun coord -> coord, triton.Get coord) >>
-        (fun (coord, ltr) -> ltr, (reachable coord ))
-    |> Map
-    |> Map.add '@' (reachable start)    
+let directDistances triton  start = 
+    explore triton start (Char.IsLower) true
 
-let explore (guide: Guide) keys =
-    let beenKey found loc = (found, loc)
+let Part1 () =
+    let triton = triton input
+    let start = triton.Find ((=) '@')
+    directDistances triton start
+    |> List.iter (printfn "%A")
 
-    let rec explore been (found: Set<char>) toFind loc dist =
-        let here = beenKey found loc
-        printfn "%A - %A - %A" 
-            loc 
-            (found |> Set.toArray |> String)
-            (Set.contains here been)
-        if Set.contains here been then Seq.singleton None else
-        let been = been.Add here
 
-        if Set.isEmpty toFind
-            then Seq.singleton (Some dist)
-        else
-            let keys, doors = guide.[loc]
-            let keysToPick = keys |> List.filter (fst >> toFind.Contains)
-            let openDoors = 
-                doors |> List.filter (fst >> Char.ToLower >> found.Contains)
-            seq{
-                yield!
-                    keysToPick
-                    |> Seq.collect (fun (key, dst) ->
-                        let found = found.Add key
-                        let toFind = toFind.Remove key
-                        explore been found toFind key (dist + dst))
-                yield!
-                    openDoors
-                    |> Seq.collect (fun (door, dst) ->
-                        explore been found toFind door (dist + dst)) }
-    explore Set.empty Set.empty (Set keys) '@' 0
-
-let Part1 () = 
-    let guide = guide test1d
-    let keys = [ 'a' .. 'i']
-    explore guide keys
-    |> Seq.choose id
-    |> Seq.head
-   
-    
 
 
 let Part2 () =
