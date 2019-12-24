0a3ba17a550e131ec3719874f46a803b79dd9198
John Donnellan <codybartfast@gmail.com>
2019-12-24 11:45:49 +0000

    Day 19 Part 1 answered

--- Day19.fs
+++ Day19.fs
@@ -9,23 +9,205 @@ let nl = Environment.NewLine
 let print obj= (printfn "%O" obj)
 let tPrint obj = (print obj); obj
 
+type Grid<'a when 'a : equality>(jagged: 'a[][]) =
+    // aoc15:18
+    let data = jagged
+    let maxX = (Array.length (data.[0])) - 1
+    let maxY = (Array.length data) - 1
+
+    let mutable formatItem = (fun x -> x.ToString())
+
+    static member Generate<'a when 'a : equality>(width, height, gen: int -> int -> 'a) =
+        [| for y in 0 .. (height - 1) do
+            [| for x in 0 .. (width - 1) do
+                gen x y |] |] 
+        |> Grid<'a> 
+        
+            
+    member _.Item
+        with get(x, y) = data.[y].[x]
+        and set(x, y) v = data.[y].[x] <- v
+
+    member _.FormatItem with get() = formatItem and set(f) = formatItem <- f
+    member this.AsText(x, y) = this.FormatItem (this.Item(x, y))
+
+    member _.Row with get(y) = data.[y]
+    member this.FormatRow = Array.map this.FormatItem >> (String.concat "")
+    member this.AsText(y) = this.FormatRow (this.Row(y))
+
+    member this.FormatGrid = Array.map this.FormatRow >> (String.concat nl)
+    member this.AsText() = this.FormatGrid data
+    override this.ToString() = this.AsText()
+    member this.Display() = printfn "%s" (this.AsText())
+    member this.TeeDisplay() = this.Display(); this
+
+    member _.InBounds(x, y) = x >= 0 && x <= maxX && y >=0 && y <= maxY
+
+    member this.Copy() = this.Transform (fun g x y -> g.[x,y])
+
+    member this.Flatern() =
+        seq{ for y in 0 .. maxY do
+                for x in 0 .. maxX do
+                     yield ((x, y), data.[y].[x]) }
+
+    member _.Coords() =
+        seq{ for y in 0 .. maxY do
+                for x in 0 .. maxX do
+                     yield (x, y) }
+
+    member this.Filter(pred) = this.Flatern() |> Seq.filter (snd >> pred)
+
+    member this.Find(pred) = this.Filter(pred) |> Seq.head |> fst
+
+    member this.NHood(x, y) =
+        [| for x in (x - 1)..(x + 1) do
+            for y in (y - 1)..(y + 1) do
+                if this.InBounds (x, y)
+                then Some this.[x,y]
+                else None |]
+
+    member this.Adjacent(x, y) =
+        let nhood = this.NHood (x, y)
+        Array.append nhood.[0 .. 3] nhood.[5 .. 8]
+
+    member this.Bordering(x, y) =
+        [| this.TryGet (x, y - 1);
+           this.TryGet (x + 1, y);
+           this.TryGet (x, y + 1);
+           this.TryGet (x - 1, y); |]
+
+    member this.Transform<'b  when 'b : equality>
+        (generate: Grid<'a> -> int -> int -> 'b) : Grid<'b> =
+            [| for y in 0 .. maxY do
+                [| for x in 0 .. maxX do
+                    generate this x y |] |]
+            |> Grid<'b>
+
+    member _.Corners() = [| (0, 0); (0, maxY); (maxX, maxY); (maxX, 0) |]
+    member this.Get((x, y)) = this.[x, y]
+    member this.Set((x, y)) value = this.[x, y] <- value
+    member this.TryGet((x, y)) =
+        match this.InBounds(x, y) with
+        | true -> Some (this.Get((x, y)))
+        | false -> None
+
+type Field = Grid<char>
+
+let fieldGrid =
+    Array.map (fun (s: string) -> s.ToCharArray())
+    >> Field
+
 let readLines day =
-    File.ReadAllLines (Path.Combine("inputs", "input" + day + ".txt"))
+    Path.Combine("inputs", "input" + day + ".txt")
+    |> File.ReadAllLines
 let lines = readLines day
+let code = lines.[0]
+let compile (str: string) = str.Split "," |> Array.map int64
 
-let parseLine (line: string) =
-    Regex.Match(line, @"(.*)")
-    |> fun (m: Match) ->
-        let grp (idx: int) = m.Groups.[idx].Value
-        let grpi = grp >> int
-        grp 1
+let computer program readInput writeOutput =
+    let mutable relBase = 0
+    let memMB = 1
+    let memory = Array.init (memMB * 1_024 * 1_024) (fun _ -> 0L)
+    let read (addr: int) = memory.[int addr]
+    let write (addr: int) (value: int64) = memory.[addr] <- value
 
-let input = lines |> Array.map parseLine
+    let halt ptr = read ptr = 99L
 
+    let posDE n = n % 100
+    let posC n = n % 1000 / 100
+    let posB n = n % 10000 / 1000
+    let posA n = n % 100000 / 10000
 
+    let opCode = read >> int >> posDE
+
+    let readArg offset modeFlag ptr =
+        let addr = ptr + offset
+        match ptr |> (read >> int >> modeFlag) with
+        | 0 -> read (int (read addr))
+        | 1 -> read addr
+        | 2 -> read ((int (read addr)) + relBase)
+        | u -> failwithf "Unexpected mode flag: %i (ptr: %i)" u ptr
+    let arg1 = readArg 1 posC
+    let arg2 = readArg 2 posB
+    let arg3 = readArg 3 posA
+
+    let writeArg offset modeFlag ptr value =
+        let addr = ptr + offset
+        match ptr |> (read >> int >> modeFlag) with
+        | 0 -> write (int (read addr)) value
+        | 1 -> failwith "Oh! Oh! Oh!"
+        | 2 -> write ((int (read addr)) + relBase) value
+        | u -> failwithf "Unexpected mode flag: %i (ptr: %i)" u ptr
+    let write1 = writeArg 1 posC
+    let write2 = writeArg 2 posB
+    let write3 = writeArg 3 posA
+
+    let add ptr = arg1 ptr + arg2 ptr |> write3 ptr;  ptr + 4
+    let mult ptr = arg1 ptr * arg2 ptr |> write3 ptr;  ptr + 4
+    let input ptr = readInput () |> write1 ptr;  ptr + 2
+    let output ptr = writeOutput (arg1 ptr);  ptr + 2
+    let jumpIfTrue ptr = if arg1 ptr <> 0L then int (arg2 ptr) else ptr + 3
+    let jumpIfFalse ptr = if arg1 ptr = 0L then int (arg2 ptr) else ptr + 3
+    let lessThan ptr =
+        (if arg1 ptr < arg2 ptr then 1L else 0L) |> write3 ptr;  ptr + 4
+    let equals ptr =
+        (if arg1 ptr = arg2 ptr then 1L else 0L) |> write3 ptr;  ptr + 4
+    let shiftRB ptr = relBase <- relBase + (int (arg1 ptr));  ptr + 2
+
+    let operation ptr =
+        match opCode ptr with
+        | 1 -> add
+        | 2 -> mult
+        | 3 -> input
+        | 4 -> output
+        | 5 -> jumpIfTrue
+        | 6 -> jumpIfFalse
+        | 7 -> lessThan
+        | 8 -> equals
+        | 9 -> shiftRB
+        | u -> failwithf "Unexpected opCode: %i (ptr: %i)" u ptr
+
+    let runToHalt () =
+        let rec run ptr =
+            if halt ptr then false else run ((operation ptr) ptr)
+        run 0
+
+    program |> Array.iteri write
+
+    runToHalt
+
+let controlInverter () =
+    let mutable (inputList: int64 list) = []
+    let mutable (resp: int64) = -2L
+
+    let provideInput = fun () ->
+        let input = List.head inputList
+        inputList <- List.tail inputList
+        input
+    let handleOutput out = resp <- out
+    let sendInstr run inputs =
+        inputList <- inputs
+        run () |> ignore
+        resp
+
+    provideInput, handleOutput, sendInstr
+
+let droneApi program (x: int) (y: int) =
+    let input, output, sendInstr = controlInverter ()
+    let run = computer program input output
+    match sendInstr run [int64 x; int64 y] with
+    | 0L -> '.'
+    | 1L -> '#'
+    | _ -> failwith "oops!"
 
 let Part1 () =
-    input
-
+    let program = compile code
+    let input, output, sendInstr = controlInverter ()
+    let run = computer program input output
+    let drone = droneApi program
+    let field = Grid.Generate (50, 50, drone)
+    field.Filter ((=) '#')
+    |> Seq.length
+    
 let Part2 () =
     ()
