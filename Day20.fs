96c08d9492f3e8cace9a2d04be57b242400318a3
John Donnellan <codybartfast@gmail.com>
2019-12-24 13:21:52 +0000

    Day 20 prep

---
+++ Day20.fs
@@ -0,0 +1,31 @@
+module Day20
+let day = "20"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let nl = Environment.NewLine
+let print obj= (printfn "%O" obj)
+let tPrint obj = (print obj); obj
+
+let readLines day =
+    File.ReadAllLines (Path.Combine("inputs", "input" + day + ".txt"))
+let lines = readLines day
+
+let parseLine (line: string) =
+    Regex.Match(line, @"(.*)")
+    |> fun (m: Match) ->
+        let grp (idx: int) = m.Groups.[idx].Value
+        let grpi = grp >> int
+        grp 1
+
+let input = lines |> Array.map parseLine
+
+
+
+let Part1 () =
+    input
+
+let Part2 () =
+    ()
