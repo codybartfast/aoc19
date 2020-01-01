2bfefcaf285dd2aa6a94a8d0b1f331185f587a03
John Donnellan <codybartfast@gmail.com>
2020-01-01 10:07:51 +0000

    Day 23 prep

--- Program.fs
+++ Program.fs
@@ -1,4 +1,4 @@
-﻿open Day22 // file needs to be included in AdventOfCode2019.fsproj
+﻿open Day23 // file needs to be included in AdventOfCode2019.fsproj
 
 let time title fn =
     let sw = System.Diagnostics.Stopwatch ()
