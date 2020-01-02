fea097314e384fde59e6195c7e8cf4bbbdc4648e
John Donnellan <codybartfast@gmail.com>
2020-01-02 15:23:19 +0000

    Day 25 Part 1 answered

--- Day25.fs
+++ Day25.fs
@@ -4,28 +4,119 @@ let day = "25"
 open System
 open System.IO
 open System.Text.RegularExpressions
+open System.Threading.Tasks
 
 let nl = Environment.NewLine
 let print obj= (printfn "%A" obj)
 let tPrint obj = (print obj); obj
 
 let readLines day =
-    File.ReadAllLines (Path.Combine("inputs", "input" + day + ".txt"))
+    Path.Combine("inputs", "input" + day + ".txt")
+    |> File.ReadAllLines
 let lines = readLines day
+let code = lines.[0]
+let compile (str: string) = str.Split "," |> Array.map int64
 
-let parseLine (line: string) =
-    Regex.Match(line, @"(.*)")
-    |> fun (m: Match) ->
-        let grp (idx: int) = m.Groups.[idx].Value
-        let grpi = grp >> int
-        grp 1
+let computer program readInput writeOutput =
+    let mutable relBase = 0
+    let memMB = 1
+    let memory = Array.init (memMB * 1_024 * 1_024) (fun _ -> 0L)
+    let read (addr: int) = memory.[int addr]
+    let write (addr: int) (value: int64) = memory.[addr] <- value
 
-let input = lines |> Array.map parseLine
+    let halt ptr = read ptr = 99L
+
+    let posDE n = n % 100
+    let posC n = n % 1000 / 100
+    let posB n = n % 10000 / 1000
+    let posA n = n % 100000 / 10000
+
+    let opCode = read >> int >> posDE
+
+    let readArg offset modeFlag ptr =
+        let addr = ptr + offset
+        match ptr |> (read >> int >> modeFlag) with
+        | 0 -> read (int (read addr))
+        | 1 -> read addr
+        | 2 -> read ((int (read addr)) + relBase)
+        | u -> failwithf "Unexpected mode flag: %i (ptr: %i)" u ptr
+    let arg1 = readArg 1 posC
+    let arg2 = readArg 2 posB
+    let arg3 = readArg 3 posA
+
+    let writeArg offset modeFlag ptr value =
+        let addr = ptr + offset
+        match ptr |> (read >> int >> modeFlag) with
+        | 0 -> write (int (read addr)) value
+        | 1 -> failwith "Oh! Oh! Oh!"
+        | 2 -> write ((int (read addr)) + relBase) value
+        | u -> failwithf "Unexpected mode flag: %i (ptr: %i)" u ptr
+    let write1 = writeArg 1 posC
+    let write2 = writeArg 2 posB
+    let write3 = writeArg 3 posA
+
+    let add ptr = arg1 ptr + arg2 ptr |> write3 ptr;  ptr + 4
+    let mult ptr = arg1 ptr * arg2 ptr |> write3 ptr;  ptr + 4
+    let input ptr = readInput () |> write1 ptr;  ptr + 2
+    let output ptr = writeOutput (arg1 ptr);  ptr + 2
+    let jumpIfTrue ptr = if arg1 ptr <> 0L then int (arg2 ptr) else ptr + 3
+    let jumpIfFalse ptr = if arg1 ptr = 0L then int (arg2 ptr) else ptr + 3
+    let lessThan ptr =
+        (if arg1 ptr < arg2 ptr then 1L else 0L) |> write3 ptr;  ptr + 4
+    let equals ptr =
+        (if arg1 ptr = arg2 ptr then 1L else 0L) |> write3 ptr;  ptr + 4
+    let shiftRB ptr = relBase <- relBase + (int (arg1 ptr));  ptr + 2
+
+    let operation ptr =
+        match opCode ptr with
+        | 1 -> add
+        | 2 -> mult
+        | 3 -> input
+        | 4 -> output
+        | 5 -> jumpIfTrue
+        | 6 -> jumpIfFalse
+        | 7 -> lessThan
+        | 8 -> equals
+        | 9 -> shiftRB
+        | u -> failwithf "Unexpected opCode: %i (ptr: %i)" u ptr
+
+    let runToHalt () =
+        let rec run ptr =
+            if halt ptr then false else run ((operation ptr) ptr)
+        run 0
+
+    program |> Array.iteri write
+    runToHalt
+
+let controlInverter () =
+    let mutable (inputList: int64 list) = []
+
+    let rec provideInput = fun () ->
+        while List.isEmpty inputList do
+            Task.Delay(100).Wait()
+        let input = List.head inputList
+        inputList <- List.tail inputList
+        input
+    let handleOutput out = Console.Write (char out)
+    let sendCommand (command: string) =
+        inputList <-
+            command.ToCharArray ()
+            |> Array.map int64
+            |> List.ofArray
+
+    provideInput, handleOutput, sendCommand
+
 
 
 
 let Part1 () =
-    input
+    let program = compile code
+    let provideInput, handleOutput, sendCommand = controlInverter ()
+    let run = computer program provideInput handleOutput
+    let runTask = Task.Run run
+    while true do 
+        let command = Console.ReadLine ()
+        sendCommand (command + "\n")
 
 let Part2 () =
     ()
